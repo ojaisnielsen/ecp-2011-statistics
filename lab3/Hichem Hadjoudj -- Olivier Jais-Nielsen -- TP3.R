c9a7b368f46cdf6ace9eb6e69b35b1e4e79f634f
Olivier Jais Nielsen <olivierj@palantir.com>
2019-04-28 10:15:00 +0200

    initial commit

---
+++ lab3/Hichem Hadjoudj -- Olivier Jais-Nielsen -- TP3.R
@@ -0,0 +1,242 @@
+﻿# TP3 : Méthodes de Classifications
+# 14 Octobre 2010 - ECP
+# Hichem HADJOUDJ - Olivier JAIS-NIELSEN
+
+###########################
+#  Analyse Discriminante  #
+###########################
+
+library(ade4)
+
+## 1 - Analyse graphique de la discrimination sepales et petales
+
+par(mfrow=c(1,2));
+s.class(iris[,1:2],iris[,5]);
+s.class(iris[,3:4],iris[,5]);
+
+# Les axes 3 et 4 (pétales) permettent de mieux classifier les individus et en particulier les setosas. Les axes 1 et 2 sont bien moins discriminants.
+
+## 2 - Analyse en composantes principales
+
+iris.pca=dudi.pca(iris[,1:4],scan=F, nf=2)
+s.class(iris.pca$li,iris[,5]) ;
+x11()
+s.corcircle(iris.pca$co); 
+
+# La largeure des sépales semble indépendante des 3 autres caractéristiques, par rapport au premier plan factoriel.
+
+## 3 - Analyse discriminante descriptive
+
+iris.d=discrimin(iris.pca,fac=iris[,5],scannf=F);
+
+# Le champ "li" fournit les projections des données sur le premier plan factoriel (le premier axe maximisant la variance inter-classes).
+# Le champ "eig" fournit les valeurs propres correspondant à chaque axe factoriel.
+# Le champ "va" fournit des grandeurs permettant d'apprécier l'information représentée par chaque axe de manière canonique (pourcentage, contrairement à la simple étude des valeurs propres) : pour chaque axe, il s'agit du rapport de la somme des distances quadratiques inter-groupes par la somme totale des distances quadratiques à la moyenne (rapport de corrélation).
+
+# Visualisation
+x11()
+s.class(iris.d$li,iris[,5]);
+
+# Analyse : Cette desciption discrimine les 3 classes.
+
+
+# Cercle des correlations
+x11()
+par(mfrow=c(1,2));
+s.corcircle(iris.pca$co);
+title(sprintf('Avant ADD'));
+s.corcircle(iris.d$va);
+title(sprintf('Après ADD'));
+# Analyse : On garde des correlations similaires au modèle ACP tout en discriminant les 3 classes.
+
+## 4 - Analyse discriminante predictive
+library(MASS)
+
+model=lda(Species~.,data=iris)
+plot(model)
+
+# Cette dernière instruction affiche pour chaque individus un point dont les coordonnées sont les valeurs des fonction discriminantes en ce point, associé à l'étiquette de sa classe réelle. On constate une bonne séparation des sétosas des autres espèces par la première fonction discriminante, alors qu'elle sépare moins franchement mais les deux autres espèces entre elles.
+
+# Echantillonage aléatoire
+i = sample(nrow(iris), 0.8 * nrow(iris))
+train = iris[i,]
+test = iris[-i,]
+
+# Analyse discriminante descriptive des données d'apprentissage
+model=lda(Species~.,data=train)
+
+# Prédiction sur les données d'apprentissage
+prediction = predict(model, train)
+
+# Matrice de confusion
+table(prediction$class, train$Species)
+
+# Prédiction sur les données de test
+prediction = predict(model, test)
+
+# Matrice de confusion
+table(prediction$class, test$Species)
+
+# Analyse: Dans les cas testés, les matrices de confusion sont quasiment diagonales, ce qui pour la prédiction sur les données d'apprentissage, valide la méthode et, pour la prédiction sur les données de test, valide le modèle.
+
+## 5 - Pouvoir discriminant d'une variable
+
+# Création d'un fonction var2 qui est une var non biaisée
+var2=function(X){
+	var2=var(X)*(length(X)-1)/length(X)
+	return(var2);
+}
+
+# Définition de la fonction permettant de calculer le pouvoir discriminant d'une variable
+PouvoirDiscrimin=function(X,Y,variable){
+	
+	nb.classe=length(unique(Y))
+	dim.total=nrow(X)
+	moyenne=mean(X[,variable])
+	var.total=var2(X[,variable])
+	
+	sum=0
+	for(j in 1:nb.classe){
+		x=c()
+		Xclasse=c()
+		for(i in 1:length(Y)){
+			if(Y[i]==unique(Y)[j]){
+				x=cbind(x,i)
+			}
+		}
+		
+		Xclasse=X[x,variable]
+		dim.classe=length(Xclasse)
+		moyenne.classe=mean(Xclasse)
+		sum=sum+(dim.classe/dim.total)*((moyenne.classe-moyenne)^2)	
+	}
+	return(sum/var.total);
+}
+
+# Calcul des pouvoirs discriminants des 4 variables
+PouvoirDiscrimin(iris[,1:4],iris[,5],1);
+PouvoirDiscrimin(iris[,1:4],iris[,5],2);
+PouvoirDiscrimin(iris[,1:4],iris[,5],3);
+PouvoirDiscrimin(iris[,1:4],iris[,5],4);
+# Analyse : on retrouve bien ce que l'on avait constaté sur le premier graphique, à savoir que les axes 3 et 4 sont bien discriminants et que l'axe 2 est le moins discriminant
+
+### Vérification de la formule de décomposition de la variance en définissant une fonction var.inter (c) et var.intra (b) puis comparaison de la somme avec la var non biaisée (c)
+
+## a - Fonction qui permet de calculer la variance inter classe
+var.inter=function(X,Y,variable){
+	
+	nb.classe=length(unique(Y))
+	dim.total=nrow(X)
+	moyenne=mean(X[,variable])
+	
+	sum=0
+	for(j in 1:nb.classe){
+		x=c()
+		Xclasse=c()
+		for(i in 1:length(Y)){
+			if(Y[i]==unique(Y)[j]){
+				x=cbind(x,i)
+			}
+		}
+		
+		Xclasse=X[x,variable]
+		dim.classe=length(Xclasse)
+		moyenne.classe=mean(Xclasse)
+		sum=sum+(dim.classe/dim.total)*((moyenne.classe-moyenne)^2)	
+	}
+	return(sum);
+}
+
+
+## b - Fonction qui permet de calculer la variance intra
+var.intra=function(X,Y,variable){
+	
+	nb.classe=length(unique(Y))
+	dim.total=nrow(X)
+	moyenne=mean(X[,variable])
+	var.total=var2(X[,variable])
+	
+	sum=0
+	for(j in 1:nb.classe){
+		x=c()
+		Xclasse=c()
+		for(i in 1:length(Y)){
+			if(Y[i]==unique(Y)[j]){
+				x=cbind(x,i)
+			}
+		}
+		
+		Xclasse=X[x,variable]
+		dim.classe=length(Xclasse)
+		sum=sum+(dim.classe/dim.total)*var2(Xclasse)	
+	}
+	return(sum);
+}
+
+
+## c - Vérification de la somme
+var2(iris[,1])
+var.inter(iris[,1:4],iris[,5],1)+var.intra(iris[,1:4],iris[,5],1);
+var2(iris[,2])
+var.inter(iris[,1:4],iris[,5],2)+var.intra(iris[,1:4],iris[,5],2);
+var2(iris[,3])
+var.inter(iris[,1:4],iris[,5],3)+var.intra(iris[,1:4],iris[,5],3);
+var2(iris[,4])
+var.inter(iris[,1:4],iris[,5],4)+var.intra(iris[,1:4],iris[,5],4);
+# Analyse : On retrouve bien l'égalité var = inter + intra
+
+### Après analyse discriminante
+
+# Après analyse
+PouvoirDiscrimin(iris.d$li,iris[,5],1)
+PouvoirDiscrimin(iris.d$li,iris[,5],2)
+# On voit que le pouvoir discriminant est meilleure sur l'axe 1 (comme illustré sur la figure) mais reste faible sur l'axe 2 (comme illustré sur la figure)
+
+
+
+
+###########################
+#  Régression logistique  #
+###########################
+
+# 1 - Chargement du fichier.
+
+setwd("/Users/hichem/Documents/Écoles/Centrale/Troisième année/Statistiques et Data Mining/TP/TP3")
+
+# Chargement des données
+tab=read.table("SAheart.txt", header=TRUE,row.names="row.names", sep=",",dec=".")[,c(1,2,3,5,7,8,9,10)];
+tab;
+
+# Interprétation : les 7 premières variables correspondent à des caractéristiques physiques, comportementales et médicales d'une population de 462 individus(matrice X de dimension n=462 x d=7), la variable chd étant l'étiquette indiquant si oui (chd=1) ou non (chd=0) l'individu a eu un incident cardiaque (vecteur Y de dimension n=462 à valeur dans {0,1})
+
+## 2 -scatterplot
+x11();
+pairs(tab,pch=22,bg=c("red","blue")[unclass(factor(tab[,"chd"]))]);
+#Analyse : a priori aucune variable ne permet de classifier de manière simple les données.
+
+## 3 - Régression logistique
+tab.glm=glm(chd~sbp+tobacco+ldl+famhist+obesity+alcohol+age,family=binomial(link="logit"),data=tab);
+anova(tab.glm,"Chisq");
+
+## 4 - Prédiction
+# Matrice de confusion
+table(tab.glm$fitted.values>0.5,tab$chd);
+
+# Calculs des pourcentages de "faux"
+Pfauxpositifs=sum((tab.glm$fitted.values>0.5)*(tab$chd==0))/sum(tab$chd==1);
+Pfauxnegatifs=sum((tab.glm$fitted.values<0.5)*(tab$chd==1))/sum(tab$chd==0);
+Pfauxpositifs;
+Pfauxnegatifs;
+
+## 5 - Validation croisée
+i=sample(nrow(iris), 0.75 * nrow(iris))
+tab.train=tab[i,];
+tab.test=tab[-i,];
+tab.train.glm=glm(chd~sbp+tobacco+ldl+famhist+obesity+alcohol+age,family=binomial(link="logit"),data=tab.train);
+tab.test.predict=predict.glm(tab.train.glm,newdata=tab.test);
+table(tab.test.predict>0.5,tab.test$chd);
+# Analyse : une telle approche permet de prédire un risque d'accident cardiaque à partir des caractéristiques d'un individu. Par exemple, on recueille les informations d'un individu et on détecte un comportement ou profil à risque. L'individu sera alors surveillé voire traité préventivement en insistant sur les facteurs de risque (choleterol, hypertension).
+
+## 6 - Variables explicatives
+tab.glm$coefficients;
+# Analyse : les variables les plus significatives sont l'historique familiale, le cholesterol (ldl) alors que les moins significatives sont de manière surprenante l'alcool et la pression sanguine.
---
+++ lab4/Hichem Hadjoudj -- Olivier Jais-Nielsen -- TP4.R
@@ -0,0 +1,417 @@
+﻿# TP4 : Méthodes de Régressions
+# 4 Novembre 2010 - ECP
+# Hichem HADJOUDJ - Olivier JAIS-NIELSEN
+
+
+# Remarque : avant toute chose, on définit le path :
+setwd("D:\\Olivier\\Documents\\Centrale S5 S6\\SDM")
+
+# Liste des documents à inclure dans le dossier source : 
+#	- immo.txt
+#	- test-immo.txt (à créer avec les valeurs à tester : "surface \n 155 \n 178 \n 200 \n 220 \n 255")
+#	- prostate.txt
+#	- UsCrime.txt
+
+## ATTENTION : il faut charger l'ensemble de l'exercice à chaque fois, pour éviter les conflits de noms
+
+##############################################################
+#	          I - Régression linéaire multiple	             #
+##############################################################
+
+### - 1 - Chargement des données
+tab=read.table('immo.txt',header=T,sep=';',dec='.')
+nrow(tab)
+# Analyse : il y a 20 observations.
+
+##############################################################
+
+### - 2 - Interprétation Graphique
+plot(tab)
+# Analyse : les nuages de points sont concentrés autour d'une direction.
+cor(tab)
+# Analyse : la matrice de corrélation présente des termes très proche de 1, ce qui confirme l'observation faite sur le graphique.
+
+##############################################################
+
+### - 3 - Modèles de regression lineaire
+# Modèle : Y représente le montant de la transaction (3ème colonne) et X les deux autres variables, on modèlise par Y/X suit une loi normale
+res=lm('transaction~.',data=tab)
+
+##  - 3.1 - Res
+res
+# Analyse : on obtient les paramètres de la régression : transaction = 309.7 + 2.6 * surface + 0.045 * valeur
+# Le prix du bien augmente de 2634 € si la surface augmente de 1 m2
+
+## - 3.2 - Res détaillé
+attributes(res)
+
+## - 3.3 - Graphique
+x11()
+plot(tab$transaction,res$fit)
+abline(a=0,b=1,col='red')
+# On constate que la regression suit assez correctement la bissectrice.
+
+## - 3.4 - Modèle réduit
+# On peut faire deux régressions à une seule variable car les corrélations initiales étaient très correctes.
+
+res1=lm('transaction~.',data=tab[,c(1,3)])
+res2=lm('transaction~.',data=tab[,c(2,3)])
+
+x11()
+plot(tab$transaction,res1$fit)
+abline(a=0,b=1,col='red')
+x11()
+plot(tab$transaction,res2$fit)
+abline(a=0,b=1,col='red')
+# Gain pratique : On peut estimer par rapport à un seul des deux paramètres.
+
+# - 3.4.1 - Prédiction
+test=read.table('test-immo.txt',header=T)
+prediction=predict.lm(res1,test)
+x11()
+plot(prediction,test$surface)
+title(sprintf('Prédictions'));
+
+##############################################################
+
+### - 4 - Test et intervalles de confiances
+summary(res)
+res$coef
+
+## - 4.1 - R2
+# R2 = Var(Y^) / Var (Y). Plus R2 est proche de 1, plus la regression est bonne.
+# Ici R2 vaut 0.8344, la regression est plutôt correcte
+# On détermine la significativité par le F-test. La p-value est de 2.3*10-7 ce qui est trËs inférieur ‡ 5%. Si le risque accepté est 5%, l'hypothèse nulle (tous les coefficients sont nuls) est rejettée; le modèle est donc significatif globalement.
+res$pvalue
+
+## - 4.2 - 
+res$coefficients
+# Les codes représentent l'ordre de grandeur des p-values associées aux coefficients. Plus le nombre d'étoiles est grand, plus la p-value est petite est donc la significativité du coefficient élevée.
+# On remarque donc que le coefficient associé à la surface est bien plus significatif que celui associé à la valeur initiale d'achat.
+
+confint(res,'surface') 
+# L'estimation donne un intervalle de confiance très large donc inutilisable.
+
+## - 4.3 -
+x11()
+plot(tab$transaction,res$residuals)
+# On remarque que la distribution des résidus est bien centrée sur 0. Ce qui est conforme avec le modèle adopté (erreur de moyenne nulle).
+
+x11()
+qqnorm(res$residuals)
+qqline(res$residuals)
+# Le normal QQ plot montre des points assez proche de la ligne théorique, ce qui tend à valider le modèle d'erreur gaussienne de moyenne nulle.
+
+
+## - 4.4 - 
+x11()
+par(mfrow=c(2,2));
+plot(res)
+
+## - 4.5 - 
+library(gplots)
+prediction=predict(res1, test, interval="confidence")
+x11()
+plotCI(test$surface, prediction[,1], ui = prediction[,3], li = prediction[,2])
+title(sprintf('Prédiction et intervalle de confiance'));
+# On constate que l'intervalle de confiance est proportionnel à la variable explicative (la surface); cela signifie que la précision est approximativement constante.
+
+
+##############################################################
+
+### - 5 - Validation croisée
+
+crossValidation<-function(trainIndices) 
+{
+	train = tab[trainIndices,]
+	test = tab[-trainIndices,]	
+	model=lm('transaction~.',data=train)
+	prediction=predict.lm(model,test)	
+	err = norm(matrix(test[,3] - prediction), type='F') / length(prediction)
+	return(err)
+}
+
+mean = 0
+for (n in 1:10)
+{
+	i = sample(nrow(tab), 0.75 * nrow(tab))	
+	mean = mean + crossValidation(i)	
+}
+mean = mean / 10
+
+mean * length(tab[,3])/ sum(tab[,3])
+
+# On obtient une erreur moyenne d'environ 2% de la valeur moyenne réelle moyenne.
+
+##############################################################
+#  F I N   D E   L A   P A R T I E   1   ( M U L T I P L E ) #
+##############################################################
+
+
+
+##############################################################
+# 	   II - Régression multiple et sélection de modèles      #
+##############################################################
+
+
+### - 0 - Chargement des données
+tab=read.table("UsCrime.txt", header=TRUE, sep="",dec=".");
+tab;
+Y=tab[,1]
+X=tab[,2:14]
+
+##############################################################
+
+### - 1 - Analyse des données
+# Il y a 47 observations, une par état américain étudié.
+plot(tab)
+# Analyse : on remarque une forte corrélation entre Ex0 et Ex1 (dépenses concernant la police pour 1959 et 1960) et une légère corrélation entre U1 et U2 (taux de chômage pour les 14-24 ans et les 35-39 ans) ainsi qu'entre W et X (relation inversée) (valeur médiane de biens et revenus transférables et nombre de famille dont les revenus sont inférieurs à la moitié du revenus moyen).
+cor(tab)
+# La matrice des corrélations confirme ces corrélations, notamment entre U1 et U2 (0.9936), entre U1 et U2 (0.7459), entre W et X (-0.8840) mais font également apparaître de nombreuses autres corrélations.
+
+##############################################################
+
+### - 2 - Modèle de régression multiple
+# Modèle explicite : Y = A0 + A.X + Eps où A est une matrice n*p et A0 et Eps sont des vecteurs n.
+
+regp=lm("R~.",data=tab)
+summary(regp)
+
+## - a - Analyse rapide du modèle : on remarque que R2 = Var(Y^) / Var (Y) vaut 0.7692, la regression est plutot correcte.
+
+## - b - Significance globale : La p-value est ici de 3.7*10-7 ce qui est très inférieur à 5%. Si le risque accepté est 5%, l'hypothèse "tous les coefficients sont nuls" est rejetée et la régression est significative globalement.
+
+## - c - Significance des coefficients : pour cela on utilise le code en étoiles. On remarque que les coefficients les plus significatifs sont Ed (éducation) et X (famille en dessous de la moitié du revenu médian) (**) suivis de Age et U2 (chômage des 35-39) (*)
+
+
+##############################################################
+
+### - 3 - Sélection de modèles
+
+## - a -
+# Fonction "step" : 
+reg=lm(R~.,data=tab); summary(reg);
+s0=step(reg); #defaut backward et AIC 
+reg0=lm(formula(s0),data=tab); summary(reg0)
+# Analyse : Cette sélection a permis une réduction du nombre de variables de 14 à 6. R2 vaut 0.7478 (contre 0.7692) ce qui montre que la régression reste de qualité correcte, la p-value est en 10-10 ce qui montre que le modèle est très significtif globalement et l'ensemble des coefficients sont très significatifs individuellement. Néanmoins il y a une perte d'informations puisque on réduit le nombre de variables du modèle.
+
+## - b -
+xnames=names(tab)[2:ncol(tab)];
+for (k in 1:length(xnames)) {
+	print(sprintf(’---> %s’,xnames[k]));
+	fk=paste("R ~", paste(xnames[-k], collapse= "+"));
+	regk=lm(as.formula(fk),data=tab);
+	print(summary(regk));
+	mes=sprintf("%5s --> %5.2f",xnames[k],AIC(regk));
+	print(mes)
+	}
+
+##############################################################
+#      F I N   D E   L A   P A R T I E   2   ( A I C )       #
+##############################################################
+
+
+
+##############################################################
+#  					III - Régression ridge		             #
+##############################################################
+
+library(MASS)
+library(ade4)
+
+### - 0 - Chargement des données
+tab=read.table("UsCrime.txt", header=TRUE, sep="",dec=".");
+Y=tab[,1]
+X=tab[,2:14]
+
+
+##############################################################
+
+### - 1 - Analyse des données et régression ridge
+# La régression de type Ridge consiste à déterminer une régression linéaire Y = X * Beta minimisant la quantité, Norm2(Y - X*Beta) + lambda * Norm2(Beta)
+# Cette méthode pénalisante peut être également vue comme une régression multiple à coefficients bornés : Y = X * Beta avec Norm2(Beta)<c
+
+# Analyse des données : Ce framedata contient 47 observations de 14 variables dont une variable que l'on souhaite expliquer : le taux de crime et 13 variables démographiques et économiques. (Il s'agit d'une étude statistique sur le nombre de crimes dans 47 états américains en 1960.
+
+##############################################################
+
+### - 2 - Régression ridge
+ridge=lm.ridge(R~.,data=tab,lambda=c(0,100))
+coef(ridge)
+# L'objet ridge$coef renvoit les coefs sur une différente échelle
+
+resmultiple=lm(R~.,data=tab)
+coef(resmultiple)
+# On retrouve bien la régression multiple simple (avec lambda=0)
+
+##############################################################
+
+### - 3 - Optimisation de la régression
+resridge=lm.ridge(R~.,data=tab,lambda=seq(0,100,0.01))
+x11()
+plot(resridge$GCV)
+x11()
+plot(resridge)
+# On constate qu'il existe une valeur de lambda pour laquelle la validation croisée est minimale.
+# Il faut retenir le modèle pour lequel lambda minimise le risque quadratique, on peut le déterminer soit à partir du tracé des coefficients ou plus facilement à partir du tracé de la validation croisée.
+lambda=as.double(names(which.min(resridge$GCV)))
+Coefridge=coef(lm.ridge(R~.,data=tab,lambda=lambda))
+
+##############################################################
+
+### - 4 - Erreur quadratique moyenne
+Yridge=as.matrix(cbind(matrix(1,47,1),X))%*%as.vector(Coefridge)
+
+Errridge=norm(as.matrix(Y) - Yridge, type='F')^2 / length(Yridge)
+Errridge
+# L'erreur quadratique moyenne est assez importante.
+
+
+##############################################################
+#     F I N   D E   L A   P A R T I E   3   ( R I D G E )    # 
+##############################################################
+
+
+##############################################################
+#  					IV - Régression Lasso    	             #
+##############################################################
+
+### - 1 - Définition
+## - 1.1 - La régression de type Lasso consiste à déterminer une régression linéaire Y = X * Beta minimisant la quantité, Norm2(Y - X*Beta) - lambda * Norm1(Beta)
+library(lars)
+
+## - 1.2 - Chargement des données
+prostate=read.table("prostate.data",header=T,dec='.', row.names = 1)
+train = subset(prostate, train==TRUE)
+test = subset(prostate, train==FALSE)
+
+Xtrain = data.matrix(train[,1:8])
+Ytrain = train[,9]
+
+Xtest = data.matrix(test[,1:8])
+Ytest = test[,9]
+
+##############################################################
+
+### - 2 - Régréssion de type Lasso
+## - 2.1 - Régression
+reslasso=lars(Xtrain,Ytrain,type="lasso")
+
+## - 2.2
+plot(reslasso)
+
+## - 2.3
+x11()
+plot(reslasso$lambda)
+
+##############################################################
+
+### - 3 - Coefficients lambda
+coef=predict.lars(reslasso,Xtrain,type="coefficients",mode="lambda",s=0)
+x11()
+barplot(coef$coefficients)
+
+##############################################################
+
+### - 4 - Affichage en fonction de lambda
+for (lambda in c(1,2,4))
+{
+	coef=predict.lars(reslasso,Xtrain,type="coefficients",mode="lambda",s=lambda)
+	x11()
+	barplot(coef$coefficients)
+}
+
+##############################################################
+
+### - 5 - Erreur quadratique
+pY=predict.lars(reslasso,Xtest,type="fit",mode="lambda",s=2)
+Errlasso = norm(data.matrix(pY$fit - Ytest), type='F')^2 / length(Ytest)
+Errlasso
+
+
+##############################################################
+#     F I N   D E   L A   P A R T I E   4   ( L A S S O )    #
+##############################################################
+
+
+##############################################################
+#  				  V - Comparaison des méthodes	             #
+##############################################################
+
+### - 1 - Partition des données
+tab=read.table("UsCrime.txt", header=TRUE, sep="",dec=".");
+i=sample(nrow(tab),0.7*nrow(tab))
+train=tab[i,]
+test=tab[-i,]
+
+##############################################################
+
+## - a - Méthode de régression multiple
+
+model=lm('R~.',data=train)
+
+predictionlm.train=predict.lm(model,train)	
+Errlm.train = norm(matrix(train[,1] - predictionlm.train), type='F')^2 / length(predictionlm.train)
+
+predictionlm.test=predict.lm(model,test)	
+Errlm.test = norm(matrix(test[,1] - predictionlm.test), type='F')^2 / length(predictionlm.test)
+
+##############################################################
+
+## - b - Méthode de régression multiple et sélection AIC
+
+reg.train=lm(R~.,data=train);
+s0=step(reg.train);
+reg0.train=lm(formula(s0),data=train);
+predictionAIC.train=predict.lm(reg0.train,train)
+ErrAIC.train=norm(matrix(train[,1] - predictionAIC.train), type='F')^2 / length(predictionAIC.train)
+
+predictionAIC.test=predict.lm(reg0.train,test)
+ErrAIC.test=norm(matrix(test[,1] - predictionAIC.test), type='F')^2 / length(predictionAIC.test)
+
+##############################################################
+
+## - c - Méthode de régression ridge
+
+resridge.train=lm.ridge(R~.,data=train,lambda=seq(0,100,0.01))
+lambda.train=as.double(names(which.min(resridge.train$GCV)))
+Coefridge.train=coef(lm.ridge(R~.,data=tab,lambda=lambda.train))
+Yridge.train=as.matrix(cbind(matrix(1,32,1),train[,2:14]))%*%as.vector(Coefridge.train)
+Errridge.train = norm(as.matrix(train[,1]) - Yridge.train, type='F')^2 / length(prediction)
+
+Yridge.test=as.matrix(cbind(matrix(1,15,1),test[,2:14]))%*%as.vector(Coefridge.train)
+Errridge.test = norm(matrix(test[,1] - Yridge.test), type='F')^2 / length(prediction)
+
+
+
+##############################################################
+
+## - d - Méthode de régression lasso
+library(lars)
+reslasso.train=lars(data.matrix(train[,2:14]),train[,1],type="lasso")
+
+lambdas.train = seq(0, 100, 0.01)
+errlasso.train = array(0, length(lambdas.train))
+for (i in 1:nrow(err.train))
+{
+	pY.train=predict.lars(reslasso.train,train[,2:14],type="fit",mode="lambda",s=lambdas.train[i])
+	errlasso.train[i] = norm(data.matrix(pY.train$fit - train[,1]), type='F')^2 / length(test[,1])
+}
+lambda.train = lambdas.train[which.min(err.train)]
+Errlasso.train=min(errlasso.train)
+
+pY.test=predict.lars(reslasso.train,test[,2:14],type="fit",mode="lambda",s=lambda.train)
+Errlasso.test = norm(data.matrix(pY.test$fit - test[,1]), type='F')^2 / length(test[,1])
+
+##############################################################
+
+### - 3 - Comparaison des modèles et conclusions
+# Nous traçon finalement les erreurs quadratiques moyennes pour chacun des modèles pour la base d'apprentissage et pour la base de test
+x11()
+barplot(c(Errlm.train,Errlm.test,ErrAIC.train,ErrAIC.test,Errridge.train,Errridge.test,Errlasso.train,Errlasso.train),col=c("orchid","orchid4","orange","orange4","slateblue","slateblue4","palegreen","palegreen4"),space=c(0,0.5,1.5,0.5,1.5,0.5,1.5,0.5),legend=c("Lm Train","Lm Test","AIC Train","AIC Test","Ridge Train","Ridge Test","Lasso Train","Lasso Test"))
+title(sprintf('Erreur quadratique moyenne par modèle'));
+
+
+##############################################################
+#        				F I N   D U   T P  	  	             #
+##############################################################
